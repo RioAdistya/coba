f2074bde0868dcdd4f773276f0659ba8e6e61a38
rioadistya <rioadistya191101@gmail.com>
2024-09-26 07:28:31 +0700

    first commit

---
+++ fun.js
@@ -0,0 +1,16 @@
+let num1 = 5;
+let num2 = 10.1;
+let sum = num1 + num2;
+console.log("hasil: " +sum);
+
+function contoh() {
+  let nilai = 100;
+  console.log(nilai);
+}
+
+contoh();
+console.log(nilai)
+
+let status = true
+
+
